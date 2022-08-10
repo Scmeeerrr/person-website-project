70efd389df3e4fd02e26563b60970d68303f6684
Scmeeerrr <ramsameer273@gmail.com>
2022-08-09 21:29:59 -0400

    final comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// setting images as variables
+// setting image in interests-hobbies as variable
 let bigImg = document.getElementById("bigImg");
 let scrollImg = document.getElementById("scrollImg");
+
+// setting image caption as variable
 let caption = document.getElementById("caption");
 
+// setting buttons as variable
 let nextButton = document.getElementById("next");
 let previousButton = document.getElementById("previous");
 
@@ -10,12 +13,14 @@ let i = 0;
 
 nextButton.onclick = function() {
     i++;
-    bigImg.src = imgArray[i];
-    caption.innerHTML = infoArray[i];
 
-    if (i == imgArray.length) {
+    if (i == imgArray.length) { // once i reaches value of length of img.array, restart from beginning
         i = 0;
         bigImg.src = imgArray[i];
+        caption.innerHTML = infoArray[i];
+    }
+    else {
+        bigImg.src = imgArray[i]; // else, continue using next element in imgArray and infoArray
         caption.innerHTML = infoArray[i];
     }
 }
@@ -23,7 +28,7 @@ nextButton.onclick = function() {
 previousButton.onclick = function() {
     i--;
 
-    if (i == -1) {
+    if (i == -1) { // similar process as nextButton, but instead works in reverse
         i = 3;
         bigImg.src = imgArray[i];
         caption.innerHTML = infoArray[i];
